c88c0edd70ca7de99ce3b0998e71586ea7846b62
Thierry Phan <thierryphan3@gmail.com>
2018-12-05 10:05:45 +0100

    Pour faire plaisir a Geoffrey

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class CategoryController
+ * @package App\Controller
+ * @Route("/category")
+ */
+class CategoryController extends AbstractController
+{
+
+    
+    /**
+     * @Route("/{id}")
+     */
+    public function index(Category $category)
+    {
+        return $this->render(
+            'category/index.html.twig',
+            [
+                'category' => $category
+            ]
+        );
+    }
+
+    public function menu()
+    {
+        $repository = $this->getdoctrine()->getRepository(Category::class);
+        $categories = $repository->findBy([],['name'=>'asc']);
+
+        return $this->render(
+            'category/menu.html.twig',
+            [
+                'categories' => $categories
+            ]
+        );
+
+    }
+}
